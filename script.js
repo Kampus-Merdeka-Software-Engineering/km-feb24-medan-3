97ab8c34b4390d1fe6e31bf52c7d59c870e3e48c
JaTri19 <jarwo.tri28@gmail.com>
2024-05-16 19:40:33 +0700

    piechart

--- script.js
+++ script.js
@@ -25,4 +25,23 @@ modeSwitch.addEventListener("click", () => {
     }
 });
 
-fetch('./json/vm_cleaned.json')
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
