1c1a1e8bc7cb5cc90e78a549dd24d6e3116e4c28
unknown <elfrnzar.gmail.com>
2024-05-20 22:32:41 +0700

    load and parse local js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-const body = document.querySelector('body'),
-    sidebar = body.querySelector('nav'),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+import cleaned from "./json/vm_cleaned.json" assert { type: "json" };
+console.log(cleaned);
 
+const body = document.querySelector("body"),
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
 
 toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-
-    }
+  sidebar.classList.toggle("close");
 });
 
-fetch('./json/vm_cleaned.json')
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light mode";
+  } else {
+    modeText.innerText = "Dark mode";
+  }
+});
+
+fetch("./json/vm_cleaned.json");
