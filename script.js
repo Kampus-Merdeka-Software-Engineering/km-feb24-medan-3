7f1f950b54872cb58c7391c1958037c6f5c9e319
unknown <elfrnzar.gmail.com>
2024-05-23 22:05:32 +0700

    isi nilai scorecard

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-import cleaned from "./json/vm_cleaned.json" assert { type: "json" };
-console.log(cleaned);
+fetch("./json/vm_cleaned.json")
+  .then((response) => response.json())
+  .then(function (json) {
+    // mengambil nilai revenue
+    const revenue = json.map(function (item) {
+      return parseFloat(item.LineTotal);
+    });
+
+    const eledatarev = document.getElementById("totalRevenue");
+    eledatarev.innerHTML = revenue.reduce((acc, curr) => acc + curr, 0);
+
+    // mengambil nilai loc
+    const loc = json.map(function (item) {
+      return item.Location;
+    });
+
+    const eledataloc = document.getElementById("dataloc");
+    eledataloc.innerHTML = new Set(loc).size;
+
+    // mengambil nilai machine
+    const mach = json.map(function (item) {
+      return item.Device_ID;
+    });
+
+    const eledatamach = document.getElementById("datamach");
+    eledatamach.innerHTML = new Set(mach).size;
+
+    //mengambil nilai category
+    const catgry = json.map(function (item) {
+      return item.Category;
+    });
+
+    const eledatacatgry = document.getElementById("datacatgry");
+    eledatacatgry.innerHTML = new Set(catgry).size;
+  });
 
 const body = document.querySelector("body"),
   sidebar = body.querySelector("nav"),
