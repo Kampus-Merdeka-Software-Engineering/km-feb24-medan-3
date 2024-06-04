26c86821ae44cf14a8ab67e090fba63a0da5c4a0
JaTri19 <115677162+JaTri19@users.noreply.github.com>
2024-06-04 21:16:18 +0700

    update js (mengeluarkan js bagian side ke file js berbeda)

--- js/script.js
+++ js/script.js
@@ -118,31 +118,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
 });
 
-// sidebar func
-const body = document.querySelector("body"),
-    sidebar = body.querySelector("nav"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
-
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-});
-
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-});
-
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
-});
 
 fetch("./json/vm_cleaned.json");
 
