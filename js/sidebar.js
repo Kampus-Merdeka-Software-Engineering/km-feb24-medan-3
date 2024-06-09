48875fd98cc3ab22db54c9fb76e21e694419aa4e
Fatimah Nur Alifiah <fatimahnalfh@gmail.com>
2024-06-09 21:02:20 +0700

    fixing dark mode halaman dataset

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,20 +1,27 @@
 // sidebar func
 const body = document.querySelector("body"),
-    sidebar = body.querySelector("nav"),
-    toggle = body.querySelector(".toggle"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+            sidebar = body.querySelector("nav"),
+            toggle = body.querySelector(".toggle"),
+            modeSwitch = body.querySelector(".toggle-switch"),
+            modeText = body.querySelector(".mode-text");
 
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-});
+        if (localStorage.getItem("dark-mode") === "enabled") {
+            body.classList.add("dark");
+            modeText.innerText = "Light mode";
+        }
 
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
+        toggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+        });
 
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
-});
+        modeSwitch.addEventListener("click", () => {
+            body.classList.toggle("dark");
+
+            if (body.classList.contains("dark")) {
+                localStorage.setItem("dark-mode", "enabled");
+                modeText.innerText = "Light mode";
+            } else {
+                localStorage.setItem("dark-mode", "disabled");
+                modeText.innerText = "Dark mode";
+            }
+        });
